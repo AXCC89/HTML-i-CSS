a78a669775bb8a418af6ee96f07c571912196452
Andrea <113423311+AXCC89@users.noreply.github.com>
2022-10-06 03:53:24 -0500

    nivel1_PHP.php
    Cambios: 
    1. Add formato.
    2. Add .php

--- nivel1_PHP.php
+++ nivel1_PHP.php
@@ -1,5 +1,5 @@
-//Ejercicio 1
 <?php
+//Ejercicio 1
 $personas = 10;
 $salary = 1.1;
 $nameUser = "Andrea";
@@ -8,35 +8,34 @@ $workshops = false;
 $performance = ($workshops = 0) ? "no":"si";
 
 echo "Trabaja con nosotros ".$nameUser," tiene un sueldo de ".$salary, " es parte de nuestro equipo de " .$personas,
-" ella ha realizado talleres ".$performance;
- ?>
+" ella ha realizado talleres ".$performance."<br />";
+
 //Ejercicio 2
-<?php
+
 $saludos = "hello word!";
-$curso = "Aquest és el curs de PHP";
-echo strtoupper($saludos),"  ", strlen ($saludos),"  ", strrev ($saludos),"  ", ($saludos.$curso);
- ?>
+$curso = " Aquest és el curs de PHP";
+echo strtoupper($saludos)."<br />".strlen ($saludos)."<br />".strrev ($saludos)."<br />".($saludos.$curso)."<br />";
+
 //Ejercicio 3
-<?php
+
 define("ANDREA");
-echo "<h1>ANDREA</h1>";
- ?>
+echo "<h1>ANDREA</h1>"."<br />";
+
 //Ejercicio 4
-<?php
+
 $x= 1;
 $y= 2;
 $n= 1.1;
 $m= 2.2;
 echo "Las varibles son: ".$x," y ". $y, " ", ", el resultado de la suma es ".($x+$y)
 , " ", ", el resultado de la resta es ".($x-$y), " ", ", el resultado del producto es ".($x*$y)
-, " ", ", el resultado del módulo es ".($x-$y), ". Las siguientes varibles son: ".$n," y ". $m, " ", ", el resultado de la suma es ".($n+$m)
+, " ", ", el resultado del módulo es ".($x-$y)."."."<br />". "Las siguientes varibles son: ".$n," y ". $m, " ", ", el resultado de la suma es ".($n+$m)
 , " ", ", el resultado de la resta es ".($n-$m), " ", ", el resultado del producto es ".($n*$m)
-, " ", ", el resultado del módulo es ".($n-$m), ". El valor del doble de cada variable: ".($x*2)," , ".($y*2)," , ".($n*2)," , ".($m*2),
-". El resultado de la suma de todas las variables: ".($x+$y+$n+$m), ", el resultado del producto de todas las variables: "
-.($x*$y*$n*$m);
- ?>
+, " ", ", el resultado del módulo es ".($n-$m)."."."<br />"."El valor del doble de cada variable: ".($x*2)," , ".($y*2)," , ".($n*2)," , ".($m*2)."."."<br />".
+"El resultado de la suma de todas las variables: ".($x+$y+$n+$m), ", el resultado del producto de todas las variables: "
+.($x*$y*$n*$m)."<br />";
+
 //Ejercicio 5
-<?php
 
   $personas = array (1,2,3,4,5);
   $coches = array (6,7,8);
@@ -46,4 +45,6 @@ echo "Las varibles son: ".$x," y ". $y, " ", ", el resultado de la suma es ".($x
   $sumtotal = array_sum($sum);
   print_r($sum);
   print_r($sumtotal);
+
  ?>
+
